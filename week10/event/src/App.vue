1004567e9904ef0b8dffcdcb3b6ab4f8ed3e7f3e
thanakrit381 <thanakrit.chi@student.mahidol.edu>
2023-03-13 15:55:49 +0700

    week10

---
+++ week10/event/src/App.vue
@@ -0,0 +1,91 @@
+<script>
+export default{
+  data(){
+    return{
+      msg: "",
+      url : "www.vuejs.org",
+      age: 21,
+      counter : 0,
+      firstName : "Deen",
+      lastName : "Phit",
+      html : "<p>Show Data</p>",
+  		isShow : true,
+      numItem : 15
+    }
+  },
+  mounted(){
+      setTimeout(()=>{
+                 this.msg = "Hello"
+                 },3000)
+  },
+  methods :{
+    getFullname : function(){
+      return this.firstName + " "+ this.lastName
+    },
+    inputFullname : function(fName,lName){
+      return fName + " "+ lName
+    },
+    upperCase : function(){
+     let upText =  this.msg.toUpperCase()
+      return upText;
+  	},
+    capitalize : function(){
+    let newMsg = this.msg
+    newMsg = newMsg.charAt(0).toUpperCase()+ newMsg.slice(1)
+    return newMsg
+  },
+    capitalWord: function(){
+      let arrMsg = this.msg.split(" ")
+      let result = ""
+      for(var word of arrMsg)
+        {
+          word = word.charAt(0).toUpperCase()+ word.slice(1)
+          result += word+" "
+        }
+      return result
+    },
+    clickMethod : function(event){
+      console.log(event)
+      alert("Congrat you win")
+    },
+    toggle : function()
+    {
+      this.isShow = !this.isShow
+    }
+  }
+}
+</script>
+<template>
+  <h1>
+    {{msg}}
+  </h1>
+  <p>
+    {{numItem}}
+  </p>
+  <p v-if="numItem <10">
+    Phease refill
+  </p>
+  <p v-else>
+    Enough stock
+  </p>
+  <p v-show = "isShow">
+    Show na
+  </p>
+  <div v-html = "html">
+    
+  </div>
+  <input type = "text" v-on:keyup = "clickMethod"><br>
+  <input type = "text" v-on:keyup.enter = "clickMethod"><br>
+  <input type = "text" v-on:keyup.space = "clickMethod"><br>
+  <input type = "text" v-on:keyup.shift.enter = "clickMethod"><br>
+  <button @click = "clickMethod">
+    ClickMe
+  </button>
+  <button @click = "toggle">
+    Toggle
+  </button>
+</template>
+<style>
+  p {
+      color : red;
+  }</style>
---
+++ week10/uppercase/src/App.vue
@@ -0,0 +1,96 @@
+<script>
+export default{
+  data(){
+    return{
+      msg: "Hello",
+      url : "www.vuejs.org",
+      age: 21,
+      counter : 0,
+      firstName : "Deen",
+      lastName : "Phit"
+    }
+  },
+  mounted(){
+    setInterval(()=>{
+      this.counter++
+    },3000)
+  },
+  methods :{
+    getFullname : function(){
+      return this.firstName + " "+ this.lastName
+    },
+    inputFullname : function(fName,lName){
+      return fName + " "+ lName
+    },
+    upperCase : function(){
+     let upText =  this.msg.toUpperCase()
+      return upText;
+  	},
+    capitalize : function(){
+    let newMsg = this.msg
+    newMsg = newMsg.charAt(0).toUpperCase()+ newMsg.slice(1)
+    return newMsg
+  },
+    capitalWord: function(){
+      let arrMsg = this.msg.split(" ")
+      let result = ""
+      for(var word of arrMsg)
+        {
+          word = word.charAt(0).toUpperCase()+ word.slice(1)
+          result += word+" "
+        }
+      return result
+    },
+    clickMethod : function(event){
+      console.log(event)
+      alert("Congrat you win")
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <a v-bind:href = "url">vuejs web</a>
+  <p>
+    <input v-model="msg">
+  </p>
+  
+  <p>
+    <input v-model="age">
+  </p>
+  <button v-on:click.stop = "clickMethod">
+    Click me
+  </button>
+  <button v-on:click.once = "clickMethod">
+    Click me once
+  </button>
+  <p v-bind:title="msg">
+    Show Title
+  </p>
+  <p>
+    {{age>20 ? "adult":"child"}}
+  </p>
+  <p>
+    Counter : {{counter}}
+  </p>
+  <p>
+    {{inputFullname("Mr1","Mrb")}}
+  </p>
+  <p>
+    {{this.getFullname()}}
+  </p>
+  <p>
+    {{upperCase()}}
+  </p>
+<p>
+    {{capitalize()}}
+  </p>
+  <p>
+    {{capitalWord()}}
+  </p>
+</template>
+<style>
+  p {
+      color : red;
+  }</style>
