b345c8065d112e2af1b48eb2e6164abcc872d45e
thanakrit381 <thanakrit.chi@student.mahidol.edu>
2023-03-20 15:58:59 +0700

    Week11 vue

---
+++ week11/vueproj/vue-project/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// import FomanticUI from 'vue-fomantic-ui'
+// import 'fomantic-ui-css/semantic.min.css'
+
+import './assets/main.css'
+
+// const app = createApp(App)
+// app.use(FomanticUI)
+
+createApp(App).mount('#app')
